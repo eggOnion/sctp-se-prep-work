d191d12782d46567cf82e47263ddcb0e2e217a04
Wei Cong <137802716+eggOnion@users.noreply.github.com>
2023-08-09 14:05:10 +0800

    Complete Moving Car Challenge

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const road = document.querySelector("#road");
+road.width = 1200;
+
+var car = document.querySelector("#car");
+
+let carPostion = 500; //How to pass the value of a CSS property into JS?
+
+window.addEventListener("keydown", (event) => {
+  console.log(event);
+
+  if (event.key === "a") {
+    //alert("Car move left");
+    carPostion = carPostion - 10;
+    car.style.left = carPostion + "px";
+    console.log(car.style.left);
+  }
+  if (event.key === "d") {
+    //alert("Car move right");
+    carPostion = carPostion + 10;
+    car.style.left = carPostion + "px";
+    console.log(car.style.left);
+  }
+});
